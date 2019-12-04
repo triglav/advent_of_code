0768eb389c45461ff0da17be086ab3ef9b93a2a9
Matej Svec <matej@msvec.sk>
2019-12-04 19:05:25 +0100

    Day 1 - part 2

--- day01.cc
+++ day01.cc
@@ -4,13 +4,25 @@ int CalculateFuel(int mass) {
   return mass / 3 - 2;
 }
 
+int CalculateFuel_correct(int mass) {
+  auto fuel = CalculateFuel(mass);
+  if (fuel <= 0) {
+    return 0;
+  }
+  return fuel + CalculateFuel_correct(fuel);
+}
+
 int main() {
   int total_fuel = 0;
+  int total_fuel_correct = 0;
   int module_mass;
   while (std::cin >> module_mass) {
     total_fuel += CalculateFuel(module_mass);
+    total_fuel_correct += CalculateFuel_correct(module_mass);
   }
-  std::cout << total_fuel << "\n";
+  std::cout
+    << total_fuel << "\n"
+    << total_fuel_correct << "\n";
   return 0;
 }
 
