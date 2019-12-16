1f6a66bc0fbcff81b89f08215c18b9e6ef3100e1
Matej Svec <matej@msvec.sk>
2019-12-17 00:04:33 +0100

    Day 16 - part 2

--- day16.cc
+++ day16.cc
@@ -4,57 +4,91 @@
 
 using Pattern = std::vector<int>;
 
-std::vector<Pattern> BuildPatterns(Pattern const &base_pattern,
-                                   size_t const size) {
-  std::vector<Pattern> patterns{size};
-  for (int i = 0; i < size; ++i) {
-    auto &p = patterns[i];
-    auto x = 0;
-    auto bi = 0;
-    while (x <= size) {
-      for (int i2 = 0; i2 <= i && x <= size; ++i2) {
-        if (x > 0) {
-          p.push_back(base_pattern[bi]);
-        }
-        ++x;
-      }
-      bi = (bi + 1) % base_pattern.size();
+int GetPatternValue(Pattern const &base_pattern, int row_idx, int element_idx) {
+  auto const y = row_idx + 1;
+  auto const t = 4 * y;
+  auto const x = (element_idx + 1) % t;
+  if (x >= 2 * y) {
+    if (x >= 3 * y) {
+      return base_pattern[3];
     }
+    return base_pattern[2];
+  } else if (x >= y) {
+    return base_pattern[1];
   }
-  return patterns;
+  return base_pattern[0];
 }
 
 std::vector<int> DoFFT(std::vector<int> const &input_list, int phase,
-                       std::vector<Pattern> const &patterns) {
+                       Pattern const &base_pattern) {
   std::vector<int> output_list;
   for (int i = 0; i < input_list.size(); ++i) {
-    auto const &p = patterns[i];
     int64_t r = 0;
     for (int i2 = 0; i2 < input_list.size(); ++i2) {
-      r += input_list[i2] * p[i2];
+      r += input_list[i2] * GetPatternValue(base_pattern, i, i2);
     }
     output_list.push_back(std::abs(r) % 10);
   }
   return output_list;
 }
 
+std::vector<int> DoFFT_partial(std::vector<int> const &input_list, int phase,
+                               Pattern const &base_pattern, int64_t offset) {
+  std::vector<int> output_list;
+  output_list.resize(input_list.size());
+
+  assert(offset > input_list.size() / 2);
+
+  int64_t s = 0;
+  for (int64_t i = offset; i < input_list.size(); ++i) {
+    s += input_list[i];
+  }
+  for (int64_t i = offset; i < input_list.size(); ++i) {
+    auto r = s;
+    s -= input_list[i];
+    output_list[i] = std::abs(r) % 10;
+  }
+  return output_list;
+}
+
 int main() {
-  std::vector<int> elements;
+  std::vector<int> base_elements;
   {
     std::string line;
     std::getline(std::cin, line);
     for (auto c : line) {
-      elements.push_back(c - '0');
+      base_elements.push_back(c - '0');
     }
   }
+  int64_t offset = 0;
+  for (int i = 0; i < 7; ++i) {
+    auto const d = base_elements[i];
+    offset = offset * 10 + d;
+  }
 
-  auto patterns = BuildPatterns({0, 1, 0, -1}, elements.size());
+  Pattern base_pattern{0, 1, 0, -1};
+  {
+    auto l = base_elements;
+    for (int i = 1; i <= 100; ++i) {
+      l = DoFFT(l, i, base_pattern);
+    }
+    for (int i = 0; i < 8; ++i) {
+      std::cout << l[i];
+    }
+    std::cout << "\n";
+  }
+
+  std::vector<int> elements;
+  for (int i = 0; i < 10000; ++i) {
+    std::copy(base_elements.begin(), base_elements.end(),
+              std::back_inserter(elements));
+  }
 
   auto l = elements;
   for (int i = 1; i <= 100; ++i) {
-    l = DoFFT(l, i, patterns);
+    l = DoFFT_partial(l, i, base_pattern, offset);
   }
-  for (int i = 0; i < 8; ++i) {
+  for (auto i = offset; i < offset + 8; ++i) {
     std::cout << l[i];
   }
   std::cout << "\n";
