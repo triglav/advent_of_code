05e9687dec2934b03862ac1aa59c647e2411863e
Matej Svec <matej@msvec.sk>
2019-12-28 18:04:01 +0100

    Day 3 (2015) - part 2

--- 2015/day03.cc
+++ 2015/day03.cc
@@ -6,29 +6,44 @@
 
 int main() {
   std::unordered_set<Coord> map;
+  std::unordered_set<Coord> map2;
 
   Coord pos{0, 0};
   map.insert(pos);
   int houses = 1;
+
+  std::array<Coord, 2> pos2{pos, pos};
+  map2.insert(pos);
+  int houses2 = 1;
+  int i = 0;
 
   char c;
   while (std::cin >> c) {
     assert(c == '^' || c == '>' || c == 'v' || c == '<');
     if (c == '^') {
       --pos.y;
+      --pos2[i].y;
     } else if (c == '>') {
       ++pos.x;
+      ++pos2[i].x;
     } else if (c == 'v') {
       ++pos.y;
+      ++pos2[i].y;
     } else if (c == '<') {
       --pos.x;
+      --pos2[i].x;
     }
 
     if (map.find(pos) == map.end()) {
       ++houses;
       map.insert(pos);
     }
+    if (map2.find(pos2[i]) == map2.end()) {
+      ++houses2;
+      map2.insert(pos2[i]);
+    }
+    i = (i + 1) % 2;
   }
-  std::cout << houses << "\n";
+  std::cout << houses << "\n" << houses2 << "\n";
   return 0;
 }
