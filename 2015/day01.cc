beb86d50e30d2b0f1d1a388ccdad76689d9905d1
Matej Svec <matej@msvec.sk>
2019-12-28 16:04:59 +0100

    Day 1 (2015) - part 2

--- 2015/day01.cc
+++ 2015/day01.cc
@@ -1,21 +1,27 @@
 #include <cassert>
 #include <iostream>
+#include <optional>
 
 int main() {
   int floor = 0;
+  std::optional<int> position;
 
+  int current_position = 0;
   char c;
   while (std::cin >> c) {
+    ++current_position;
+    assert(c == '(' || c == ')');
     if (c == '(') {
       ++floor;
-      continue;
-    } else if (c == ')') {
+    } else {
       --floor;
-      continue;
     }
-    assert(false);
+    if (floor == -1 && !position.has_value()) {
+      position = current_position;
+    }
   }
   std::cout << floor << "\n";
+  assert(position.has_value());
+  std::cout << *position << "\n";
   return 0;
 }
-
