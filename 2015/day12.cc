19e156f01958492d8795a8e56c8f8d430212465e
Matej Svec <matej@msvec.sk>
2019-12-29 15:27:46 +0100

    Day 12 (2015) - part 2

--- 2015/day12.cc
+++ 2015/day12.cc
@@ -17,11 +17,30 @@ int64_t Sum(json const &j) {
   return r;
 }
 
+int64_t Sum2(json const &j) {
+  if (j.is_object()) {
+    for (auto const &x : j) {
+      if (x.is_string() && x.get<std::string>() == "red") {
+        return 0;
+      }
+    }
+  }
+  int64_t r = 0;
+  for (auto const &x : j) {
+    if (x.is_number()) {
+      r += x.get<int64_t>();
+    } else if (x.is_object() || x.is_array()) {
+      r += Sum2(x);
+    }
+  }
+  return r;
+}
+
 int main() {
   json j;
   std::cin >> j;
 
-  std::cout << Sum(j) << "\n";
+  std::cout << Sum(j) << "\n" << Sum2(j) << "\n";
 
   return 0;
 }
