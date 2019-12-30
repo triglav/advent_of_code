276dc1166ecf1c6a0075f945d3d57ee21f7cdb26
Matej Svec <matej@msvec.sk>
2019-12-30 14:37:47 +0100

    Day 14 (2015) - part 2

--- 2015/day14.cc
+++ 2015/day14.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 
@@ -19,10 +20,37 @@ int CalculateDistanceAfterDuration(Reindeer const &r, int total_duration) {
          std::min(y, r.flight_duration) * r.flight_speed;
 }
 
+int Simulate(std::vector<Reindeer> const &reindeers, int total_duration) {
+  std::vector<int> points(reindeers.size(), 0);
+
+  for (int k = 1; k <= total_duration; ++k) {
+    int max_distance = 0;
+    std::vector<size_t> indices;
+
+    for (int i = 0; i < reindeers.size(); ++i) {
+      auto const d = CalculateDistanceAfterDuration(reindeers[i], k);
+      if (d >= max_distance) {
+        if (d > max_distance) {
+          max_distance = d;
+          indices.clear();
+        }
+        indices.push_back(i);
+      }
+    }
+    for (auto i : indices) {
+      points[i] += 1;
+    }
+  }
+  auto it = std::max_element(points.begin(), points.end());
+  return *it;
+}
+
 int main() {
   int const kTotalDuration = 2503;
 
   int max_distance = 0;
+
+  std::vector<Reindeer> reindeers;
 
   std::string line;
   while (std::getline(std::cin, line)) {
@@ -33,10 +61,13 @@ int main() {
         sv2number<int>(t[6]),
         sv2number<int>(t[13]),
     };
-
     auto const distance = CalculateDistanceAfterDuration(r, kTotalDuration);
     max_distance = std::max(distance, max_distance);
+
+    reindeers.emplace_back(std::move(r));
   }
-  std::cout << max_distance << "\n";
+  std::cout << max_distance << "\n"
+            << Simulate(reindeers, kTotalDuration) << "\n";
+
   return 0;
 }
