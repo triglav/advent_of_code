78b6fe3bb632f8c289baef148b2aa01d83cce7bf
Matej Svec <matej@msvec.sk>
2019-12-30 17:57:58 +0100

    Day 17 (2015) - part 2

--- 2015/day17.cc
+++ 2015/day17.cc
@@ -21,13 +21,21 @@ int main() {
   }
 
   int count = 0;
+  int min_container_count = std::numeric_limits<int>::max();
+  int ways_count = 0;
   for (auto const &c : container_combinations) {
     auto const sum = std::accumulate(c.begin(), c.end(), 0);
     if (sum == kLiters) {
       ++count;
+      if (c.size() <= min_container_count) {
+        if (c.size() < min_container_count) {
+          min_container_count = c.size();
+          ways_count = 0;
+        }
+        ++ways_count;
+      }
     }
   }
-  std::cout << count << "\n";
-
+  std::cout << count << "\n" << ways_count << "\n";
   return 0;
 }
