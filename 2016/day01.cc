db35ad7815250d720276c3c8c4cc7d6cf81e376c
Matej Svec <matej.svec@iblsoft.com>
2021-12-02 23:42:50 +0100

    Day 1 (2016) - part 2

--- 2016/day01.cc
+++ 2016/day01.cc
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <limits>
 #include <sstream>
 #include <stdexcept>
 #include <string>
-#include <tuple>
+#include <unordered_map>
+
+#include "coord.h"
 
 enum class Direction {
   N = 0,
@@ -13,33 +16,49 @@ enum class Direction {
 };
 
 constexpr Direction TurnRight(Direction d) {
-  return static_cast<Direction>((static_cast<int>(d) + 1) % static_cast<int>(Direction::Count));
+  return static_cast<Direction>((static_cast<int>(d) + 1) %
+                                static_cast<int>(Direction::Count));
 }
 constexpr Direction TurnLeft(Direction d) {
-  return static_cast<Direction>((static_cast<int>(d) - 1 + static_cast<int>(Direction::Count)) %
+  return static_cast<Direction>(
+      (static_cast<int>(d) - 1 + static_cast<int>(Direction::Count)) %
       static_cast<int>(Direction::Count));
 }
 
-constexpr std::tuple<int, int> WalkForward(std::tuple<int, int> pos, Direction d, int blocks) {
+constexpr Coord WalkForward(Coord pos, Direction d, int blocks) {
   auto const [x, y] = pos;
   switch (d) {
   case Direction::N:
-    return std::make_tuple(x, y-blocks);
+    return Coord{x, y - blocks};
   case Direction::W:
-    return std::make_tuple(x+blocks, y);
+    return Coord{x + blocks, y};
   case Direction::S:
-    return std::make_tuple(x, y+blocks);
+    return Coord{x, y + blocks};
   case Direction::E:
-    return std::make_tuple(x-blocks, y);
+    return Coord{x - blocks, y};
   default:
     throw std::logic_error("invalid direction");
   }
 }
 
+Coord WalkForwardAndMarkVisited(std::unordered_map<Coord, int> *visited,
+                                Coord pos, Direction d, int blocks) {
+  auto p = pos;
+  for (int i = 0; i < blocks; ++i) {
+    p = WalkForward(p, d, 1);
+    auto const it = visited->find(p);
+    auto const c = (it != visited->end() ? it->second : 0);
+    visited->insert_or_assign(p, c + 1);
+  }
+  return p;
+}
 
 int main() {
   auto direction = Direction::N;
-  auto position = std::make_tuple(0, 0);
+  auto position = Coord{0, 0};
+
+  std::unordered_map<Coord, int> visited;
+  visited.insert({position, 1});
 
   std::string token;
   while (std::cin >> token) {
@@ -57,9 +76,23 @@ int main() {
 
     std::string blocks;
     std::getline(ss, blocks, ',');
-    position = WalkForward(position, direction, std::stoi(blocks));
+    position = WalkForwardAndMarkVisited(&visited, position, direction,
+                                         std::stoi(blocks));
   }
   auto const [x, y] = position;
   std::cout << std::abs(x) + std::abs(y) << "\n";
+
+  int distance = std::numeric_limits<int>::max();
+  for (auto i = visited.begin(); i != visited.end(); ++i) {
+    if (i->second < 2) {
+      continue;
+    }
+    auto const p = i->first;
+    auto const d = std::abs(p.x) + std::abs(p.y);
+    if (d < distance) {
+      distance = d;
+    }
+  }
+  std::cout << distance << "\n";
   return 0;
 }
