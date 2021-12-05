ff9c718b57dcc61815daeca8e1cb8e30c1f13c8c
Matej Svec <matej.svec@iblsoft.com>
2021-12-05 23:59:05 +0100

    Day 3 (2021) - part 2

--- 2021/day03.cc
+++ 2021/day03.cc
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,6 +10,14 @@ void NoteOnes(std::vector<int> *bit_count, std::string token) {
       (*bit_count)[i] += 1;
     }
   }
+}
+
+std::vector<int> GetBitCount(std::vector<std::string> const &numbers) {
+  std::vector<int> r(numbers.front().size(), 0);
+  for (auto const &t : numbers) {
+    NoteOnes(&r, t);
+  }
+  return r;
 }
 
 uint64_t GetMostCommon(std::vector<int> const &bit_count, int count) {
@@ -30,20 +40,72 @@ uint64_t GetLeastCommon(std::vector<int> const &bit_count, int count) {
   return r;
 }
 
-int main() {
-  std::string token;
-  std::cin >> token;
-
-  std::vector<int> bit_count(token.size(), 0);
-  NoteOnes(&bit_count, token);
-  int count = 1;
-  while (std::cin >> token) {
-    NoteOnes(&bit_count, token);
-    ++count;
+uint64_t BinaryInStringToNumber(std::string const &s) {
+  uint64_t r = 0;
+  for (auto b : s) {
+    r <<= 1;
+    r += ((b == '1') ? 1 : 0);
   }
+  return r;
+}
+
+uint64_t FindOxygenGeneratorRating(std::vector<std::string> const &numbers,
+                                   size_t idx) {
+  auto const bit_count = GetBitCount(numbers);
+  auto const most_common =
+      ((bit_count[idx] >= (numbers.size() / 2 + numbers.size() % 2)) ? '1'
+                                                                     : '0');
+  std::vector<std::string> matching;
+  std::remove_copy_if(
+      numbers.begin(), numbers.end(), std::back_inserter(matching),
+      [idx, most_common](auto t) { return t[idx] != most_common; });
+
+  if (matching.size() > 1) {
+    return FindOxygenGeneratorRating(
+        matching, ((idx + 1 < numbers.front().size()) ? idx + 1 : 0));
+  }
+  assert(matching.size() == 1);
+  return BinaryInStringToNumber(matching.front());
+}
+
+uint64_t FindCo2ScrubberRating(std::vector<std::string> const &numbers,
+                               size_t idx) {
+  auto const bit_count = GetBitCount(numbers);
+  auto const least_common =
+      ((bit_count[idx] < (numbers.size() / 2 + numbers.size() % 2)) ? '1'
+                                                                    : '0');
+
+  std::vector<std::string> matching;
+  std::remove_copy_if(
+      numbers.begin(), numbers.end(), std::back_inserter(matching),
+      [idx, least_common](auto t) { return t[idx] != least_common; });
+  if (matching.size() > 1) {
+    return FindCo2ScrubberRating(
+        matching, ((idx + 1 < numbers.front().size()) ? idx + 1 : 0));
+  }
+  assert(matching.size() == 1);
+  return BinaryInStringToNumber(matching.front());
+}
+
+int main() {
+  std::vector<std::string> numbers;
+
+  std::string token;
+  while (std::cin >> token) {
+    numbers.push_back(token);
+  }
+
+  auto const bit_count = GetBitCount(numbers);
+  auto const count = numbers.size();
+
   auto const gammaRate = GetMostCommon(bit_count, count);
   auto const epsilonRate = GetLeastCommon(bit_count, count);
   auto const powerConsumption = gammaRate * epsilonRate;
   std::cout << powerConsumption << "\n";
+
+  auto const oxygenGeneratorRating = FindOxygenGeneratorRating(numbers, 0);
+  auto const co2ScrubberRating = FindCo2ScrubberRating(numbers, 0);
+  auto const lifeSupportRating = oxygenGeneratorRating * co2ScrubberRating;
+  std::cout << lifeSupportRating << "\n";
   return 0;
 }
