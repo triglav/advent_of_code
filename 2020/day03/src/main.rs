dd69d00dc185628b46b963bafb9a63d84a85c3c3
Matej Svec <matej@msvec.sk>
2022-07-15 23:44:51 +0200

    Day 3 (2020) - part 2

--- 2020/day03/src/main.rs
+++ 2020/day03/src/main.rs
@@ -16,16 +16,32 @@ fn main() {
                 .collect::<Vec<bool>>()
         })
         .collect::<Vec<Vec<bool>>>();
-    let map_y = map.len();
-    let map_x = map[0].len();
 
-    let (slope_x, slope_y) = (3, 1);
-    let r = (0..map.len())
-        .scan((0, 0), |pos, i| {
-            *pos = (slope_x * i, slope_y * i);
-            Some(*pos)
-        })
-        .filter(|(x, y)| map[*y % map_y][*x % map_x])
-        .count();
+    fn check_slope(map: &Vec<Vec<bool>>, slope: (usize, usize)) -> usize {
+        let map_y = map.len();
+        let map_x = map[0].len();
+        let (slope_x, slope_y) = slope;
+        (0..(map.len() / slope_y))
+            .scan((0, 0), |pos, i| {
+                *pos = (slope_x * i, slope_y * i);
+                Some(*pos)
+            })
+            .filter(|(x, y)| map[*y % map_y][*x % map_x])
+            .count()
+    }
+
+    let r = check_slope(&map, (3, 1));
     println!("{}", r);
+
+    let r2 = [
+        check_slope(&map, (1, 1)),
+        r,
+        check_slope(&map, (5, 1)),
+        check_slope(&map, (7, 1)),
+        check_slope(&map, (1, 2)),
+    ]
+    .into_iter()
+    .reduce(|a, i| a * i)
+    .unwrap();
+    println!("{}", r2);
 }
