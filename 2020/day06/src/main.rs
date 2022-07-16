660fbac67903b8bdd34472d19fe4d74bcffe48d2
Matej Svec <matej@msvec.sk>
2022-07-16 19:28:11 +0200

    Day 6 (2020) - part 2

--- 2020/day06/src/main.rs
+++ 2020/day06/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::HashSet,
+    collections::{HashMap, HashSet},
     io::{self, BufRead},
 };
 
@@ -7,18 +7,38 @@ fn main() {
     let stdin = io::stdin();
     let lines = stdin.lock().lines().map(|l| l.unwrap());
 
-    let mut c = 0;
+    let mut r = 0;
     let mut test = HashSet::<u8>::new();
+
+    let mut r2 = 0;
+    let mut test2 = HashMap::<u8, u32>::new();
+    let mut p = 0;
+
     for l in lines {
         if l.is_empty() {
-            c += test.len();
+            r += test.len();
             test.clear();
+
+            r2 += test2.values().filter(|c| **c == p).count();
+            p = 0;
+            test2.clear();
             continue;
         }
+        p += 1;
         for c in l.as_bytes() {
             test.insert(*c);
+            match test2.get_mut(c) {
+                Some(v) => {
+                    *v += 1;
+                }
+                None => {
+                    test2.insert(*c, 1);
+                }
+            };
         }
     }
-    c += test.len();
-    println!("{}", c);
+    r += test.len();
+    r2 += test2.values().filter(|c| **c == p).count();
+    println!("{}", r);
+    println!("{}", r2);
 }
