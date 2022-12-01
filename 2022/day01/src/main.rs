7d104afd43c9043402e828c490118b382edd089d
Matej Svec <matej@msvec.sk>
2022-12-01 23:54:58 +0100

    Day 1 (2022) - part 2

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,7 +1,8 @@
-use std::{cmp::max, io};
+use std::{cmp::max, io, collections::BinaryHeap};
 
 fn main() {
     let mut r: u32 = 0;
+    let mut r2 = BinaryHeap::new();
     let mut c: u32 = 0;
     for line in io::stdin().lines() {
         match line {
@@ -9,11 +10,15 @@ fn main() {
                 Ok(n) => c += n,
                 Err(_) => {
                     r = max(r, c);
+                    r2.push(c);
                     c = 0;
                 }
             },
             Err(_) => panic!("Invalid input"),
         }
     }
+    r = max(r, c);
+    r2.push(c);
     println!("{}", r);
+    println!("{}", r2.pop().unwrap() + r2.pop().unwrap() + r2.pop().unwrap());
 }
