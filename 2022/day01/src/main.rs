5ab1e225600de847c2c48de605413e5eab2f68db
Matej Svec <matej@msvec.sk>
2022-12-02 01:01:00 +0100

    Day 1 (2022) - functionsss

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,24 +1,25 @@
-use std::{cmp::max, io, collections::BinaryHeap};
+use itertools::Itertools;
+use std::io;
+
+fn keep_max3(mut v: Vec<u32>, i: u32) -> Vec<u32> {
+    let idx = v.binary_search(&i).unwrap_or_else(|idx| idx);
+    v.insert(idx, i);
+    if v.len() > 3 {
+        v.remove(0);
+    }
+    v
+}
 
 fn main() {
-    let mut r: u32 = 0;
-    let mut r2 = BinaryHeap::new();
-    let mut c: u32 = 0;
-    for line in io::stdin().lines() {
-        match line {
-            Ok(t) => match t.parse::<u32>() {
-                Ok(n) => c += n,
-                Err(_) => {
-                    r = max(r, c);
-                    r2.push(c);
-                    c = 0;
-                }
-            },
-            Err(_) => panic!("Invalid input"),
-        }
-    }
-    r = max(r, c);
-    r2.push(c);
-    println!("{}", r);
-    println!("{}", r2.pop().unwrap() + r2.pop().unwrap() + r2.pop().unwrap());
+    let top3 = io::stdin()
+        .lines()
+        .map(|l| l.unwrap().parse::<u32>().unwrap_or(0))
+        .coalesce(|a, b| if b > 0 { Ok(a + b) } else { Err((a, 0)) })
+        .fold(Vec::<u32>::with_capacity(4), keep_max3);
+
+    let r1 = top3.last().unwrap();
+    println!("{}", r1);
+
+    let r2 = top3.iter().sum::<u32>();
+    println!("{}", r2);
 }
