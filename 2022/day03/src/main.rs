de0081473b1dcbcf96b62e04c59b35c388fa15ab
Matej Svec <matej@msvec.sk>
2022-12-03 17:12:16 +0100

    Day 3 (2022) - part 2

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::{collections::HashSet, io};
 
 fn item_priority(i: char) -> u32 {
@@ -22,10 +23,18 @@ fn find_common_items(a: &str, b: &str) -> HashSet<char> {
     })
 }
 
+fn note_items(items: &String) -> Vec<i32> {
+    items.chars().fold(vec![0; 52], |mut a, c| {
+        a[(item_priority(c) - 1) as usize] = 1;
+        a
+    })
+}
+
 fn main() {
-    let r1 = io::stdin()
-        .lines()
-        .map(|l| l.unwrap())
+    let lines = io::stdin().lines().map(|l| l.unwrap()).collect_vec();
+
+    let r1 = lines
+        .iter()
         .map(|l| {
             let (a, b) = l.split_at(l.len() / 2);
             find_common_items(a, b)
@@ -36,4 +45,20 @@ fn main() {
         .sum::<u32>();
 
     println!("{}", r1);
+
+    let chunks = &lines.into_iter().chunks(3);
+    let r2 = chunks.into_iter().map(|chunks| {
+        let zz = chunks
+            .map(|c| note_items(&c))
+            .reduce(|a, b| {
+                a.iter()
+                    .zip(b.iter())
+                    .map(|(ia, ib)| *ia + *ib)
+                    .collect_vec()
+            })
+            .unwrap();
+        (zz.iter().position(|i| *i == 3).unwrap() + 1) as u32
+    }).sum::<u32>();
+
+    println!("{}", r2);
 }
