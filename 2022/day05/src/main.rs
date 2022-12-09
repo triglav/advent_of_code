d4a809140ba1bc23405c4713e573daff67c0379b
Matej Svec <matej@msvec.sk>
2022-12-09 23:59:26 +0100

    Day 5 (2022) - part 2

--- 2022/day05/src/main.rs
+++ 2022/day05/src/main.rs
@@ -25,6 +25,18 @@ fn rearrange(stacks: &mut Vec<Stack>, count: usize, from_idx: usize, to_idx: usi
     });
 }
 
+fn rearrange2(stacks: &mut Vec<Stack>, count: usize, from_idx: usize, to_idx: usize) {
+    let mut temp = Stack::new();
+    (0..count).for_each(|_| {
+        let c = stacks[from_idx].pop().unwrap();
+        temp.push(c);
+    });
+    (0..count).for_each(|_| {
+        let c = temp.pop().unwrap();
+        stacks[to_idx].push(c);
+    });
+}
+
 fn main() {
     let lines = io::stdin()
         .lines()
@@ -33,7 +45,7 @@ fn main() {
     let idx = lines.iter().position(|l| l == "").unwrap();
     let stack_count = (lines[idx - 1].len() + 1) / 4;
 
-    let mut stacks = (0..=(idx - 2))
+    let stacks = (0..=(idx - 2))
         .rev()
         .map(|i| &lines[i])
         .map(|l| parse_config_line(l, stack_count))
@@ -45,11 +57,27 @@ fn main() {
             });
             a
         });
-    ((idx + 1)..lines.len())
+    let procedure = ((idx + 1)..lines.len())
         .map(|idx| parse_procedure_line(&lines[idx]))
-        .for_each(|(count, from_idx, to_idx)| {
-            rearrange(&mut stacks, count, from_idx, to_idx);
-        });
-    let r1 = stacks.iter().map(|s| s.last().unwrap()).collect::<String>();
+        .collect::<Vec<_>>();
+
+    let mut stacks1 = stacks.to_owned();
+    procedure.iter().for_each(|(count, from_idx, to_idx)| {
+        rearrange(&mut stacks1, *count, *from_idx, *to_idx);
+    });
+    let r1 = stacks1
+        .iter()
+        .map(|s| s.last().unwrap())
+        .collect::<String>();
     println!("{}", r1);
+
+    let mut stacks2 = stacks.to_owned();
+    procedure.iter().for_each(|(count, from_idx, to_idx)| {
+        rearrange2(&mut stacks2, *count, *from_idx, *to_idx);
+    });
+    let r2 = stacks2
+        .iter()
+        .map(|s| s.last().unwrap())
+        .collect::<String>();
+    println!("{}", r2);
 }
