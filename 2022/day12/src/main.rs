41280af0bbc6db492a473809abb3195636f4c399
Matej Svec <matej@msvec.sk>
2022-12-12 09:26:32 +0100

    Day 12 (2022) - part 2

--- 2022/day12/src/main.rs
+++ 2022/day12/src/main.rs
@@ -57,11 +57,13 @@ fn get_neighbours(c: Coord, width: usize, height: usize) -> Vec<Coord> {
     v
 }
 
-fn main() {
-    let (grid, start, end) = read_grid();
-    let height = grid.len();
-    let width = grid[0].len();
-
+fn map_moves(
+    grid: &Grid,
+    width: usize,
+    height: usize,
+    start: Coord,
+    reverse: bool,
+) -> HashMap<Coord, u32> {
     let mut moves = HashMap::<Coord, u32>::new();
     moves.insert(start, 0);
 
@@ -75,13 +77,55 @@ fn main() {
 
         for n in get_neighbours(c, width, height) {
             let ne = grid[n.y][n.x];
-            if !moves.contains_key(&n) && ne <= e + 1 {
-                moves.insert(n, v + 1);
-                todo.push_back(n);
+            if !moves.contains_key(&n) {
+                match reverse {
+                    false => {
+                        if ne <= e + 1 {
+                            moves.insert(n, v + 1);
+                            todo.push_back(n);
+                        }
+                    }
+                    true => {
+                        if e <= ne || e - 1 == ne {
+                            moves.insert(n, v + 1);
+                            todo.push_back(n);
+                        }
+                    }
+                }
             }
         }
     }
+    moves
+}
 
-    let r1 = moves.get(&end).unwrap();
+fn find_lowest(grid: &Grid, width: usize, height: usize) -> Vec<Coord> {
+    let mut v = vec![];
+    for y in 0..height {
+        for x in 0..width {
+            if grid[y][x] == 0 {
+                v.push(Coord { x, y });
+            }
+        }
+    }
+    v
+}
+
+fn main() {
+    let (grid, start, end) = read_grid();
+    let height = grid.len();
+    let width = grid[0].len();
+
+    let moves1 = map_moves(&grid, width, height, start, false);
+    let r1 = moves1.get(&end).unwrap();
     println!("{}", r1);
+
+    let moves2 = map_moves(&grid, width, height, end, true);
+    let starts2 = find_lowest(&grid, width, height);
+    let r2 = starts2
+        .iter()
+        .filter(|s| moves2.contains_key(s))
+        .map(|s| moves2.get(s).unwrap())
+        .min()
+        .unwrap();
+    println!("{}", r2);
 }
