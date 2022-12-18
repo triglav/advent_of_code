7356fa25248fe044f8629971da1fcb275649c482
Matej Svec <matej@msvec.sk>
2022-12-18 17:13:31 +0100

    Day 17 (2022) - part 2

--- 2022/day17/src/main.rs
+++ 2022/day17/src/main.rs
@@ -1,4 +1,4 @@
-use std::{cmp::max, io, ops};
+use std::{cmp::max, collections::HashMap, io, ops};
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 struct Coord {
@@ -99,17 +99,21 @@ enum Jet {
     Right,
 }
 
-struct Chamber {
+type Situation = (usize, usize, Vec<usize>);
+
+struct Chamber<'a> {
     width: i32,
-    height: i32,
+    height: usize,
     rock_count: usize,
     grid: Vec<Vec<bool>>,
-    jet_pattern: Vec<Jet>,
+    jet_pattern: &'a Vec<Jet>,
     next_jet_idx: usize,
+
+    height_per_column: Vec<usize>,
 }
 
-impl Chamber {
-    fn new(width: i32, jet_pattern: Vec<Jet>) -> Self {
+impl<'a> Chamber<'a> {
+    fn new(width: i32, jet_pattern: &'a Vec<Jet>) -> Self {
         Self {
             width,
             height: 0,
@@ -117,6 +121,7 @@ impl Chamber {
             grid: vec![],
             jet_pattern,
             next_jet_idx: 0,
+            height_per_column: vec![0; width as usize],
         }
     }
 
@@ -126,13 +131,16 @@ impl Chamber {
         }
     }
 
-    fn spawn_rock(&mut self) -> Rock {
+    fn spawn_rock(&mut self) -> (Rock, Situation) {
         let rock_shape = self.rock_count % 5;
         let pos = Coord::new(2, self.height as i32 + 3);
         let rock = Rock::new(pos, rock_shape);
         self.rock_count += 1;
         self.enlarge_grid(pos.y as usize + 3);
-        rock
+
+        let top_offsets = self.get_top_offsets(pos.y as usize);
+        let situation = (rock_shape, self.next_jet_idx, top_offsets);
+        (rock, situation)
     }
 
     fn collides(&self, pos: Coord, shape: &Shape) -> bool {
@@ -188,12 +196,14 @@ impl Chamber {
                 .unwrap();
             assert_eq!(*p, false);
             *p = true;
-            self.height = max(self.height, c.y + 1);
+            self.height = max(self.height, c.y as usize + 1);
+            let column_height = self.height_per_column.get_mut(c.x as usize).unwrap();
+            *column_height = max(*column_height, c.y as usize + 1);
         }
     }
 
-    fn simulate_rock(&mut self) {
-        let mut rock = self.spawn_rock();
+    fn simulate_rock(&mut self) -> Situation {
+        let (mut rock, situation) = self.spawn_rock();
         loop {
             self.push_rock_by_jet(&mut rock);
             if !self.apply_gravity(&mut rock) {
@@ -201,6 +211,11 @@ impl Chamber {
                 break;
             }
         }
+        situation
+    }
+
+    fn get_top_offsets(&self, y: usize) -> Vec<usize> {
+        self.height_per_column.iter().map(|h| y - h).collect()
     }
 }
 
@@ -220,10 +235,36 @@ fn main() {
             _ => panic!("Invalid pattern type `{}`", c),
         })
         .collect();
-    let mut c = Chamber::new(7, jet_pattern);
+    let mut c = Chamber::new(7, &jet_pattern);
     for _ in 0..2022 {
         c.simulate_rock();
     }
     let r1 = c.height;
     println!("{}", r1);
+
+    let mut c2 = Chamber::new(7, &jet_pattern);
+    let mut previous_situations = HashMap::<Situation, (usize, usize)>::new();
+    let ((rc0, h0), (rc1, h1)) = loop {
+        let s = c2.simulate_rock();
+        if let Some(prev) = previous_situations.insert(s, (c2.rock_count, c2.height)) {
+            break (prev, (c2.rock_count, c2.height));
+        }
+    };
+
+    let rocks2 = 1000000000000_usize;
+
+    let d_r = rc1 - rc0;
+    let d_h = h1 - h0;
+
+    let ratio = (rocks2 - rc0) / d_r;
+    let h2 = ratio * d_h;
+
+    let rc3 = (rocks2 - rc0) % d_r;
+    for _ in 0..rc3 {
+        c2.simulate_rock();
+    }
+    let h3 = c2.height - h1;
+
+    let r2 = h0 + h2 + h3;
+    println!("{}", r2);
 }
