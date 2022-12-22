51a2c3cba7137710a2e496db8a301f875f9b1ed0
Matej Svec <matej@msvec.sk>
2022-12-22 23:01:20 +0100

    Day 21 (2022) - part 2

--- 2022/day21/src/main.rs
+++ 2022/day21/src/main.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashMap, io};
+use std::{
+    collections::{HashMap, HashSet},
+    io,
+};
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 enum Operation<'a> {
@@ -26,14 +29,127 @@ fn parse_monkey<'a>(s: &'a String) -> (&'a str, Operation<'a>) {
     (name, op)
 }
 
-fn evaluate(name: &str, operations: &HashMap<&str, Operation>) -> i64 {
-    let op = operations.get(name).unwrap();
-    match *op {
-        Operation::Plus(a, b) => evaluate(a, operations) + evaluate(b, operations),
-        Operation::Minus(a, b) => evaluate(a, operations) - evaluate(b, operations),
-        Operation::Multiply(a, b) => evaluate(a, operations) * evaluate(b, operations),
-        Operation::Divide(a, b) => evaluate(a, operations) / evaluate(b, operations),
+fn evaluate<'a>(
+    name: &'a str,
+    ops: &'a HashMap<&str, Operation>,
+    res: &mut HashMap<&'a str, i64>,
+) -> i64 {
+    let op = ops.get(name).unwrap();
+    let r = match *op {
+        Operation::Plus(a, b) => evaluate(a, ops, res) + evaluate(b, ops, res),
+        Operation::Minus(a, b) => evaluate(a, ops, res) - evaluate(b, ops, res),
+        Operation::Multiply(a, b) => evaluate(a, ops, res) * evaluate(b, ops, res),
+        Operation::Divide(a, b) => evaluate(a, ops, res) / evaluate(b, ops, res),
         Operation::Value(a) => a,
+    };
+    res.insert(name, r);
+    r
+}
+
+fn get_op_params<'a>(op: &'a Operation) -> Option<(&'a str, &'a str)> {
+    match op {
+        Operation::Plus(a, b) => Some((a, b)),
+        Operation::Minus(a, b) => Some((a, b)),
+        Operation::Multiply(a, b) => Some((a, b)),
+        Operation::Divide(a, b) => Some((a, b)),
+        Operation::Value(_) => None,
+    }
+}
+
+fn map_humn_branch<'a>(name: &'a str, ops: &'a HashMap<&str, Operation>) -> Vec<&'a str> {
+    fn map_humn_branch_rec<'b>(
+        name: &'b str,
+        ops: &'b HashMap<&str, Operation>,
+        humn: &mut Vec<&'b str>,
+    ) -> bool {
+        if name == "humn" {
+            humn.push(name);
+            return true;
+        }
+        let op = ops.get(name).unwrap();
+        match get_op_params(op) {
+            Some((a, b)) => {
+                if map_humn_branch_rec(a, ops, humn) || map_humn_branch_rec(b, ops, humn) {
+                    humn.push(name);
+                    true
+                } else {
+                    false
+                }
+            }
+            None => false,
+        }
+    }
+    let mut humn_branch = Vec::<&str>::new();
+    map_humn_branch_rec(name, ops, &mut humn_branch);
+    humn_branch
+}
+
+fn identify_unknown<'a>((a, b): (&'a str, &'a str), humn: &'a HashSet<&str>) -> (&'a str, &'a str) {
+    if !humn.contains(a) {
+        assert!(humn.contains(b));
+        (a, b)
+    } else {
+        assert!(!humn.contains(b));
+        (b, a)
+    }
+}
+
+fn find_humn_value(
+    expected: i64,
+    to_solve: &str,
+    ops: &HashMap<&str, Operation>,
+    res: &HashMap<&str, i64>,
+    humn: &HashSet<&str>,
+) -> i64 {
+    if to_solve == "humn" {
+        return expected;
+    }
+    match ops.get(to_solve).unwrap() {
+        Operation::Plus(a, b) => {
+            let (known, unknown) = identify_unknown((*a, *b), humn);
+            let known_value = res.get(known).unwrap();
+            let unknown_value = expected - known_value;
+            find_humn_value(unknown_value, unknown, ops, res, humn)
+        }
+        Operation::Minus(a, b) => {
+            // r = a - b
+            if humn.contains(a) {
+                // a = r + b
+                assert!(!humn.contains(b));
+                let b_value = res.get(b).unwrap();
+                let a_value = expected + b_value;
+                find_humn_value(a_value, a, ops, res, humn)
+            } else {
+                // b = a - r
+                assert!(humn.contains(b));
+                let a_value = res.get(a).unwrap();
+                let b_value = a_value - expected;
+                find_humn_value(b_value, b, ops, res, humn)
+            }
+        }
+        Operation::Multiply(a, b) => {
+            let (known, unknown) = identify_unknown((*a, *b), humn);
+            let known_value = res.get(known).unwrap();
+            let unknown_value = expected / known_value;
+            find_humn_value(unknown_value, unknown, ops, res, humn)
+        }
+        Operation::Divide(a, b) => {
+            // r = a / b
+            if humn.contains(a) {
+                // a = r * b
+                assert!(!humn.contains(b));
+                let b_value = res.get(b).unwrap();
+                let a_value = expected * b_value;
+                find_humn_value(a_value, a, ops, res, humn)
+            } else {
+                // b = a / r
+                assert!(humn.contains(b));
+                let a_value = res.get(a).unwrap();
+                let b_value = a_value / expected;
+                find_humn_value(b_value, b, ops, res, humn)
+            }
+        }
+        Operation::Value(_) => panic!("Unexpected opration!"),
     }
 }
 
@@ -41,6 +157,20 @@ fn main() {
     let lines: Vec<_> = io::stdin().lines().map(|l| l.unwrap()).collect();
     let operations: HashMap<&str, Operation> = lines.iter().map(|l| parse_monkey(&l)).collect();
 
-    let r1 = evaluate("root", &operations);
-    println!("{:?}", r1);
+    let mut results = HashMap::<&str, i64>::new();
+
+    let r1 = evaluate("root", &operations, &mut results);
+    println!("{}", r1);
+
+    let root = operations.get("root").unwrap();
+    let (root_a, root_b) = get_op_params(root).unwrap();
+
+    let humn_branch = map_humn_branch(root_a, &operations)
+        .into_iter()
+        .collect::<HashSet<&str>>();
+
+    let (known, unknown) = identify_unknown((root_a, root_b), &&humn_branch);
+    let expected = *results.get(known).unwrap();
+    let r2 = find_humn_value(expected, unknown, &operations, &results, &humn_branch);
+    println!("{}", r2);
 }
