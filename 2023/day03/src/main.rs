5c21307ca4ed161574be847a2e6aa73ba9bba760
Matej Svec <matej@msvec.sk>
2023-12-03 17:24:23 +0100

    Day 3 (2023) - part 2

--- 2023/day03/src/main.rs
+++ 2023/day03/src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{collections::BTreeMap, io};
 
 use itertools::Itertools;
 
@@ -39,6 +39,44 @@ fn has_adjacent_symbol(grid: &Vec<Vec<char>>, x: usize, y: usize, l: usize) -> b
     false
 }
 
+fn is_gear(c: char) -> bool {
+    c == '*'
+}
+
+fn get_gears(grid: &Vec<Vec<char>>, x: usize, y: usize, l: usize) -> Vec<(usize, usize)> {
+    let height = grid.len();
+    let width = grid[0].len();
+
+    let mut gears = vec![];
+    for i in (x as i32 - 1).max(0)..(x + l + 1).min(width) as i32 {
+        if (y as i32 - 1) >= 0 {
+            let c = grid[y - 1][i as usize];
+            if is_gear(c) {
+                gears.push((i as usize, y - 1));
+            }
+        }
+        if y + 1 < height {
+            let c = grid[y + 1][i as usize];
+            if is_gear(c) {
+                gears.push((i as usize, y + 1));
+            }
+        }
+    }
+    if (x as i32 - 1) >= 0 {
+        let c = grid[y][x - 1];
+        if is_gear(c) {
+            gears.push((x - 1, y));
+        }
+    }
+    if x + l < width {
+        let c = grid[y][x + l];
+        if is_gear(c) {
+            gears.push((x + l, y));
+        }
+    }
+    gears
+}
+
 #[derive(Debug)]
 struct Number {
     pub value: u32,
@@ -55,7 +93,7 @@ fn main() {
     let height = grid.len();
     let width = grid[0].len();
 
-    let r1 = (0..height)
+    let part_numbers = (0..height)
         .flat_map(|y| {
             let mut numbers = vec![];
             let mut x = 0;
@@ -75,11 +113,29 @@ fn main() {
         })
         .filter_map(|n| {
             if has_adjacent_symbol(&grid, n.x, n.y, n.l) {
-                Some(n.value)
+                Some(n)
             } else {
                 None
             }
         })
-        .sum::<u32>();
+        .collect_vec();
+    let r1 = part_numbers.iter().map(|n| n.value).sum::<u32>();
     println!("{}", r1);
+
+    let r2 = part_numbers
+        .iter()
+        .flat_map(|n| {
+            get_gears(&grid, n.x, n.y, n.l)
+                .into_iter()
+                .map(|g| (n.value, g))
+        })
+        .fold(BTreeMap::new(), |mut acc, (n, g)| {
+            acc.entry(g).or_insert(vec![]).push(n);
+            acc
+        })
+        .into_iter()
+        .filter(|(_k, v)| v.len() == 2)
+        .map(|(_k, v)| v[0] as u64 * v[1] as u64)
+        .sum::<u64>();
+    println!("{:?}", r2);
 }
