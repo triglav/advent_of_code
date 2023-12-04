38bc2080be62c249fbfc704f63c9d4337246db49
Matej Svec <matej@msvec.sk>
2023-12-04 15:04:01 +0100

    Day 4 (2023) - part 2

--- 2023/day04/src/main.rs
+++ 2023/day04/src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{collections::HashMap, io};
 
 struct Card {
     pub id: u32,
@@ -28,11 +28,16 @@ fn parse(line: &str) -> Card {
     }
 }
 
-fn count_winning(numbers: &[u32], winning: &[u32]) -> u32 {
+fn count_matching(numbers: &[u32], winning: &[u32]) -> u32 {
     let count = numbers
         .iter()
         .filter_map(|n| winning.binary_search(n).ok())
         .count() as u32;
+    count
+}
+
+fn count_winning(numbers: &[u32], winning: &[u32]) -> u32 {
+    let count = count_matching(numbers, winning);
     match count {
         0 => 0u32,
         1 => 1u32,
@@ -41,9 +46,32 @@ fn count_winning(numbers: &[u32], winning: &[u32]) -> u32 {
 }
 
 fn main() {
-    let cards = io::stdin().lines().map(|l| parse(l.unwrap().as_str()));
+    let cards = io::stdin()
+        .lines()
+        .map(|l| parse(l.unwrap().as_str()))
+        .collect::<Vec<_>>();
     let r1 = cards
+        .iter()
         .map(|c| count_winning(&c.numbers, &c.winning))
         .sum::<u32>();
     println!("{}", r1);
+
+    let ids = cards.iter().map(|c| c.id).collect::<Vec<_>>();
+    let mut m = HashMap::<u32, u32>::new();
+    ids.iter().for_each(|id| {
+        m.insert(*id, 1);
+    });
+    let mut grcka = 0u32;
+    cards.iter().for_each(|c| {
+        let w0 = *m.get(&c.id).unwrap();
+        let w = count_matching(&c.numbers, &c.winning);
+        (c.id + 1..=c.id + w).for_each(|i| {
+            *m.get_mut(&i).unwrap() += w0;
+        });
+        if w == 0 && grcka == 0 {
+            grcka = c.id;
+        }
+    });
+    let r2 = m.values().sum::<u32>();
+    println!("{}", r2);
 }
