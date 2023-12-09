6eb308ccf5680a2d51f76f0d9b0c8ec4f12656dd
Matej Svec <matej@msvec.sk>
2023-12-09 22:39:22 +0100

    Day 7 (2023) - part 2

--- 2023/day07/src/main.rs
+++ 2023/day07/src/main.rs
@@ -1,5 +1,7 @@
 use std::{collections::HashMap, io};
 
+use itertools::Itertools;
+
 #[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
 enum HandType {
     FiveOfAKind = 0,
@@ -14,7 +16,9 @@ enum HandType {
 #[derive(Debug)]
 struct Hand {
     pub hand_type: HandType,
+    pub hand_type2: HandType,
     pub strength: u32,
+    pub strength2: u32,
     pub bid: u32,
 }
 
@@ -37,15 +41,42 @@ fn get_hand_type(cards: &str) -> HandType {
     }
 }
 
-fn parse(line: &str, card_strength: &HashMap<char, u32>) -> Hand {
+fn get_hand_type2(cards: &str) -> HandType {
+    let s0: String = cards.chars().filter(|c| *c != 'J').collect();
+    let l = cards.len() - s0.len();
+    if l == 0 {
+        return get_hand_type(cards);
+    }
+    let cards = "AKQT98765432";
+    (0..l)
+        .map(|_| cards.chars())
+        .multi_cartesian_product()
+        .map(|v| {
+            let s = v.into_iter().collect::<String>();
+            let s = format!("{s0}{s}");
+            get_hand_type(&s)
+        })
+        .min()
+        .unwrap()
+}
+
+fn parse(
+    line: &str,
+    card_strength: &HashMap<char, u32>,
+    card_strength2: &HashMap<char, u32>,
+) -> Hand {
     let mut t = line.split(' ');
     let s = t.next().unwrap();
     let bid = t.next().unwrap().parse::<u32>().unwrap();
     Hand {
         hand_type: get_hand_type(s),
+        hand_type2: get_hand_type2(s),
         strength: s
             .chars()
             .fold(0u32, |a, c| a * 13 + card_strength.get(&c).unwrap()),
+        strength2: s
+            .chars()
+            .fold(0u32, |a, c| a * 13 + card_strength2.get(&c).unwrap()),
         bid,
     }
 }
@@ -66,10 +97,25 @@ fn main() {
         ('3', 1u32),
         ('2', 0u32),
     ]);
+    let card_strength2 = HashMap::from([
+        ('A', 12u32),
+        ('K', 11u32),
+        ('Q', 10u32),
+        ('T', 9u32),
+        ('9', 8u32),
+        ('8', 7u32),
+        ('7', 6u32),
+        ('6', 5u32),
+        ('5', 4u32),
+        ('4', 3u32),
+        ('3', 2u32),
+        ('2', 1u32),
+        ('J', 0u32),
+    ]);
 
     let mut hands = io::stdin()
         .lines()
-        .map(|l| parse(l.unwrap().as_str(), &card_strength))
+        .map(|l| parse(l.unwrap().as_str(), &card_strength, &card_strength2))
         .collect::<Vec<_>>();
 
     hands.sort_by(|a, b| {
@@ -85,4 +131,18 @@ fn main() {
         .map(|(i, h)| (i as u32 + 1) * h.bid)
         .sum::<u32>();
     println!("{}", r1);
+
+    hands.sort_by(|a, b| {
+        a.hand_type2
+            .cmp(&b.hand_type2)
+            .then_with(|| b.strength2.cmp(&a.strength2))
+    });
+
+    let r2 = hands
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(i, h)| (i as u32 + 1) * h.bid)
+        .sum::<u32>();
+    println!("{}", r2);
 }
