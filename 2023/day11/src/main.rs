ed17e1911f03e07cf93b724df2039e8d2814942f
Matej Svec <matej@msvec.sk>
2023-12-11 08:35:01 +0100

    Day 11 (2023) - part 2

--- 2023/day11/src/main.rs
+++ 2023/day11/src/main.rs
@@ -2,6 +2,36 @@ use std::{collections::HashSet, io};
 
 use itertools::{iproduct, Itertools};
 
+fn calculate_galaxies(
+    galaxies: &[(usize, usize)],
+    is_row_expandable: &HashSet<usize>,
+    is_col_expandable: &HashSet<usize>,
+    expand_distance: u64,
+) -> u64 {
+    galaxies
+        .iter()
+        .combinations(2)
+        .map(|c| {
+            assert_eq!(c.len(), 2);
+            let x1 = c[0].0.min(c[1].0);
+            let x2 = c[0].0.max(c[1].0);
+            let y1 = c[0].1.min(c[1].1);
+            let y2 = c[0].1.max(c[1].1);
+
+            let expandable_x = (x1..=x2)
+                .filter(|&x| is_col_expandable.contains(&x))
+                .count() as u64;
+            let expandable_y = (y1..=y2)
+                .filter(|&y| is_row_expandable.contains(&y))
+                .count() as u64;
+
+            let x = (x2 - x1) as u64 + expandable_x * (expand_distance - 1);
+            let y = (y2 - y1) as u64 + expandable_y * (expand_distance - 1);
+            x + y
+        })
+        .sum::<u64>()
+}
+
 fn main() {
     let grid = io::stdin()
         .lines()
@@ -22,27 +52,9 @@ fn main() {
         .filter(|(col, row)| grid[*row][*col] == '#')
         .collect::<Vec<_>>();
 
-    let r1 = galaxies
-        .iter()
-        .combinations(2)
-        .map(|c| {
-            assert_eq!(c.len(), 2);
-            let x1 = c[0].0.min(c[1].0);
-            let x2 = c[0].0.max(c[1].0);
-            let y1 = c[0].1.min(c[1].1);
-            let y2 = c[0].1.max(c[1].1);
-
-            let expandable_x = (x1..=x2)
-                .filter(|&x| is_col_expandable.contains(&x))
-                .count();
-            let expandable_y = (y1..=y2)
-                .filter(|&y| is_row_expandable.contains(&y))
-                .count();
-
-            let x = x2 - x1 + expandable_x;
-            let y = y2 - y1 + expandable_y;
-            x + y
-        })
-        .sum::<usize>();
+    let r1 = calculate_galaxies(&galaxies, &is_row_expandable, &is_col_expandable, 2);
     println!("{}", r1);
+
+    let r2 = calculate_galaxies(&galaxies, &is_row_expandable, &is_col_expandable, 1000000);
+    println!("{}", r2);
 }
