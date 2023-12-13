e4eb7c21144b50e1b5e7ae91d3d1fd87210f5e84
Matej Svec <matej@msvec.sk>
2023-12-13 16:47:49 +0100

    Day 13 (2023) - part 2

--- 2023/day13/src/main.rs
+++ 2023/day13/src/main.rs
@@ -42,6 +42,22 @@ impl Grid {
             }
         })
     }
+
+    fn fix_smudge(&self) -> impl Iterator<Item = Self> + '_ {
+        (0..self.grid.len()).map(|i| {
+            let mut g2 = self.grid.clone();
+            g2[i] = match g2[i] {
+                '#' => '.',
+                '.' => '#',
+                _ => panic!("invalid char"),
+            };
+            Grid {
+                width: self.width,
+                height: self.height,
+                grid: g2,
+            }
+        })
+    }
 }
 
 #[derive(Debug)]
@@ -50,8 +66,24 @@ struct Pattern {
     pub vertical_bitmap: Vec<u64>,
 }
 
+fn find_reflection_detail(bitmaps: &[u64]) -> Vec<usize> {
+    let reflections = bitmaps
+        .iter()
+        .enumerate()
+        .tuple_windows()
+        .filter_map(|((i, a), (_, b))| if a == b { Some(i + 1) } else { None })
+        .filter(|&c| {
+            let l = &bitmaps[0..c];
+            let r = &bitmaps[c..];
+            let is_perfect_reflection = l.iter().rev().zip(r.iter()).all(|(a, b)| a == b);
+            is_perfect_reflection
+        })
+        .collect::<Vec<usize>>();
+    reflections
+}
+
 impl Pattern {
-    fn from_grid(grid: Grid) -> Self {
+    fn from(grid: &Grid) -> Self {
         let horizontal_bitmap = (0..grid.height)
             .map(|i| grid.get_row_bitmap(i))
             .collect::<Vec<_>>();
@@ -64,38 +96,45 @@ impl Pattern {
         }
     }
 
-    fn from(lines: Vec<String>) -> Self {
-        let grid = Grid::from(lines);
-        Self::from_grid(grid)
-    }
-
     fn find_reflection(&self) -> (Option<usize>, Option<usize>) {
-        fn find_reflection_detail(bitmaps: &[u64]) -> Option<usize> {
-            let reflections = bitmaps
-                .iter()
-                .enumerate()
-                .tuple_windows()
-                .filter_map(|((i, a), (_, b))| if a == b { Some(i + 1) } else { None })
-                .filter(|&c| {
-                    let l = &bitmaps[0..c];
-                    let r = &bitmaps[c..];
-                    let is_perfect_reflection = l.iter().rev().zip(r.iter()).all(|(a, b)| a == b);
-                    is_perfect_reflection
-                })
-                .collect::<Vec<usize>>();
-            assert!(reflections.len() <= 1);
-            reflections.into_iter().next()
-        }
-
         let h = find_reflection_detail(&self.horizontal_bitmap);
         let v = find_reflection_detail(&self.vertical_bitmap);
-        assert_ne!(h.is_some(), v.is_some());
-        (h, v)
+        assert!(
+            h.len() + v.len() <= 1,
+            "too many reflections {:?} {:?}",
+            h,
+            v
+        );
+        (h.into_iter().next(), v.into_iter().next())
+    }
+
+    fn find_fixed_reflection(
+        &self,
+        r0: (Option<usize>, Option<usize>),
+    ) -> (Option<usize>, Option<usize>) {
+        let h = find_reflection_detail(&self.horizontal_bitmap);
+        let v = find_reflection_detail(&self.vertical_bitmap);
+
+        let h = h
+            .into_iter()
+            .filter(|&h| h != r0.0.unwrap_or(0))
+            .collect::<Vec<_>>();
+        let v = v
+            .into_iter()
+            .filter(|&v| v != r0.1.unwrap_or(0))
+            .collect::<Vec<_>>();
+        assert!(
+            h.len() + v.len() <= 1,
+            "too many reflections {:?} {:?}",
+            h,
+            v
+        );
+        (h.into_iter().next(), v.into_iter().next())
     }
 }
 
 fn main() {
-    let r1 = io::stdin()
+    let grids = io::stdin()
         .lines()
         .map(|l| l.unwrap())
         .fold(vec![vec![]], |mut a, b| {
@@ -107,6 +146,11 @@ fn main() {
             a
         })
         .into_iter()
+        .map(Grid::from)
+        .collect::<Vec<_>>();
+
+    let r1 = grids
+        .iter()
         .map(Pattern::from)
         .map(|p| match p.find_reflection() {
             (Some(h), None) => h as u64 * 100,
@@ -115,4 +159,29 @@ fn main() {
         })
         .sum::<u64>();
     println!("{:?}", r1);
+
+    let r2 = grids
+        .iter()
+        .map(|g| {
+            let r0 = Pattern::from(g).find_reflection();
+
+            let patterns = g.fix_smudge().map(|g| Pattern::from(&g));
+            let reflections = patterns
+                .filter_map(|p| {
+                    let r = p.find_fixed_reflection(r0);
+                    // println!("{:?}", r);
+                    match r {
+                        (Some(h), None) => Some(h as u64 * 100),
+                        (None, Some(v)) => Some(v as u64),
+                        (None, None) => None,
+                        _ => panic!("invalid pattern"),
+                    }
+                })
+                .unique()
+                .collect::<Vec<_>>();
+            assert!(reflections.len() <= 1, "too many ? {:?}", reflections);
+            reflections.into_iter().next().unwrap()
+        })
+        .sum::<u64>();
+    println!("{:?}", r2);
 }
