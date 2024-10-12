99e6d8b8a902b0b521d41f0557b1516fbc544df0
Matej Svec <matej@msvec.sk>
2024-10-13 00:10:36 +0200

    Day 3 (2016) - part 2

--- 2016/day03/day03.go
+++ 2016/day03/day03.go
@@ -46,8 +46,54 @@ func solve1(lines []string) int {
 	return r
 }
 
+func solve2(lines []string) int {
+	t1 := make(chan int)
+	t2 := make(chan int)
+	t3 := make(chan int)
+	go func() {
+		for _, line := range lines {
+			f := strings.Fields(line)
+			if len(f) != 3 {
+				panic("unexpected input")
+			}
+			e1, _ := strconv.Atoi(f[0])
+			e2, _ := strconv.Atoi(f[1])
+			e3, _ := strconv.Atoi(f[2])
+			t1 <- e1
+			t2 <- e2
+			t3 <- e3
+		}
+		close(t1)
+		close(t2)
+		close(t3)
+	}()
+
+	validate := func(c, o chan int) {
+		r := 0
+		for {
+			a := <-c
+			b := <-c
+			c, ok := <-c
+			if validateTriangle(a, b, c) {
+				r += 1
+			}
+			if !ok {
+				break
+			}
+		}
+		o <- r
+	}
+
+	o := make(chan int)
+	go validate(t1, o)
+	go validate(t2, o)
+	go validate(t3, o)
+	return <-o + <-o + <-o
+}
+
 func main() {
 	input := bufio.NewScanner(os.Stdin)
 	lines := ReadAllLines(input)
 	fmt.Println(solve1(lines))
+	fmt.Println(solve2(lines))
 }
