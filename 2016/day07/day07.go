2806dd5fd76d5075fd2df81eade0b5296b8fc14f
Matej Svec <matej@msvec.sk>
2024-10-13 18:39:18 +0200

    Day 7 (2016) - part 2

--- 2016/day07/day07.go
+++ 2016/day07/day07.go
@@ -24,11 +24,11 @@ func HasABBA(s string) bool {
 	return false
 }
 
-func SupportsTLS(s string) bool {
+func splitSequences(s string) (supernet, hypernet []string) {
 	t1 := strings.Split(s, "[")
 
-	regular := []string{t1[0]}
-	hypernet := []string{}
+	supernet = []string{t1[0]}
+	hypernet = []string{}
 
 	for _, t := range t1[1:] {
 		t2 := strings.Split(t, "]")
@@ -36,16 +36,60 @@ func SupportsTLS(s string) bool {
 			panic(fmt.Sprintf("unexpected token length: %d\n%s", len(t2), t2))
 		}
 		hypernet = append(hypernet, t2[0])
-		regular = append(regular, t2[1])
+		supernet = append(supernet, t2[1])
 	}
+	return
+}
 
+func SupportsTLS(s string) bool {
+	supernet, hypernet := splitSequences(s)
 	for _, h := range hypernet {
 		if HasABBA(h) {
 			return false
 		}
 	}
-	for _, h := range regular {
+	for _, h := range supernet {
 		if HasABBA(h) {
+			return true
+		}
+	}
+	return false
+}
+
+func FindABA(supernet []string) []string {
+	aba := []string{}
+	for _, s := range supernet {
+		for i := 0; i < len(s)-2; i++ {
+			if s[i+0] != s[i+1] && s[i+0] == s[i+2] {
+				aba = append(aba, s[i+0:i+3])
+			}
+		}
+	}
+	return aba
+}
+
+func ABA2BAB(aba string) string {
+	bab := []byte{aba[1], aba[0], aba[1]}
+	return string(bab)
+}
+
+func HasBAB(hypernet []string, bab string) bool {
+	for _, s := range hypernet {
+		for i := 0; i < len(s)-2; i++ {
+			if s[i+0] == bab[0] && s[i+1] == bab[1] && s[i+2] == bab[2] {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func SupportsSSL(s string) bool {
+	supernet, hypernet := splitSequences(s)
+	abas := FindABA(supernet)
+	for _, aba := range abas {
+		bab := ABA2BAB(aba)
+		if HasBAB(hypernet, bab) {
 			return true
 		}
 	}
@@ -62,8 +106,19 @@ func solve1(lines []string) int {
 	return count
 }
 
+func solve2(lines []string) int {
+	count := 0
+	for _, l := range lines {
+		if SupportsSSL(l) {
+			count += 1
+		}
+	}
+	return count
+}
+
 func main() {
 	input := bufio.NewScanner(os.Stdin)
 	lines := ReadAllLines(input)
 	fmt.Println(solve1(lines))
+	fmt.Println(solve2(lines))
 }
--- 2016/day07/day07_test.go
+++ 2016/day07/day07_test.go
@@ -23,3 +23,21 @@ func TestSupportsTLS(t *testing.T) {
 		})
 	}
 }
+
+func TestSupportsSSL(t *testing.T) {
+	testCases := []struct {
+		in   string
+		want bool
+	}{
+		{in: "aba[bab]xyz", want: true},
+		{in: "xyx[xyx]xyx", want: false},
+		{in: "aaa[kek]eke", want: true},
+		{in: "zazbz[bzb]cdb", want: true},
+	}
+	for _, tC := range testCases {
+		t.Run("solve1", func(t *testing.T) {
+			got := SupportsSSL(tC.in)
+			assert.Equal(t, tC.want, got)
+		})
+	}
+}
