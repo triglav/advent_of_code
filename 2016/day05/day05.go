4d690eaefaf02ecac072a612ab81a5e569c6865d
Matej Svec <matej@msvec.sk>
2024-10-13 15:21:45 +0200

    Day 5 (2016)

---
+++ 2016/day05/day05.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+)
+
+func makeHash(key string, index int) string {
+	s := []byte(fmt.Sprintf("%s%d", key, index))
+	hash := md5.Sum(s)
+	return hex.EncodeToString(hash[:])
+}
+
+func startsWith5Zeroes(hash string) bool {
+	return hash[0] == '0' &&
+		hash[1] == '0' &&
+		hash[2] == '0' &&
+		hash[3] == '0' &&
+		hash[4] == '0'
+}
+
+func solve1(line string) string {
+	var password []byte
+	for index := 0; len(password) < 8; index++ {
+		h := makeHash(line, index)
+		if startsWith5Zeroes(h) {
+			password = append(password, h[5])
+		}
+	}
+	return string(password)
+}
+
+func main() {
+	line := "reyedfim"
+	fmt.Println(solve1(line))
+}
---
+++ 2016/day05/day05_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidateMakeHash(t *testing.T) {
+	testCases := []struct {
+		key, want string
+		index     int
+	}{
+		{
+			key: "abc", index: 3231929,
+			want: "00000155f8105dff7f56ee10fa9b9abd",
+		},
+		{
+			key: "abc", index: 5017308,
+			want: "000008f82c5b3924a1ecbebf60344e00",
+		},
+		{
+			key: "abc", index: 5278568,
+			want: "00000f9a2c309875e05c5a5d09f1b8c4",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run("makeHash", func(t *testing.T) {
+			got := makeHash(tC.key, tC.index)
+			assert.Equal(t, tC.want, got)
+		})
+	}
+}
+
+func TestValidateSolve1(t *testing.T) {
+	testCases := []struct {
+		in, want string
+	}{
+		{
+			in:   "abc",
+			want: "18f47a30",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run("solve1", func(t *testing.T) {
+			got := solve1(tC.in)
+			assert.Equal(t, tC.want, got)
+		})
+	}
+}
