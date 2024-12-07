876c47f9789f9f4e61d5825900023c1f1406694e
Matej Svec <matej@msvec.sk>
2024-12-07 18:40:38 +0100

    Day 3 (2024) - part 2

--- 2024/day03/src/main.rs
+++ 2024/day03/src/main.rs
@@ -3,20 +3,37 @@ use std::io;
 use regex::Regex;
 
 fn main() {
-    let lines = io::stdin().lines().map(|l| l.unwrap());
+    let input = io::stdin()
+        .lines()
+        .map(|l| l.unwrap())
+        .collect::<Vec<_>>()
+        .join("");
 
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    //
-    let r1 = lines
-        .flat_map(|l| {
-            re.captures_iter(&l)
-                .map(|m| {
-                    let a = m[1].parse::<i32>().unwrap();
-                    let b = m[2].parse::<i32>().unwrap();
-                    a * b
-                })
-                .collect::<Vec<_>>()
+
+    let r1 = re
+        .captures_iter(&input)
+        .map(|m| {
+            let a = m[1].parse::<i32>().unwrap();
+            let b = m[2].parse::<i32>().unwrap();
+            a * b
         })
         .sum::<i32>();
     println!("{}", r1);
+
+    let re_do = Regex::new(r"do\(\)").unwrap();
+    let re_dont = Regex::new(r"don't\(\)").unwrap();
+
+    let r2 = re_do
+        .split(&input)
+        .flat_map(|s| re_dont.split(s).take(1))
+        .flat_map(|s| {
+            re.captures_iter(s).map(|m| {
+                let a = m[1].parse::<i32>().unwrap();
+                let b = m[2].parse::<i32>().unwrap();
+                a * b
+            })
+        })
+        .sum::<i32>();
+    println!("{:?}", r2);
 }
