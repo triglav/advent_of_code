6da58b974560128b090f6cbf25313bb0225ebf16
Matej Svec <matej@msvec.sk>
2024-12-08 22:11:00 +0100

    Day 8 (2024) - part 2

--- 2024/day08/src/main.rs
+++ 2024/day08/src/main.rs
@@ -56,6 +56,33 @@ fn get_antinodes(a: (i64, i64), b: (i64, i64)) -> Vec<(i64, i64)> {
     vec![c, d]
 }
 
+fn get_antinodes2(a: (i64, i64), b: (i64, i64), width: i64, height: i64) -> Vec<(i64, i64)> {
+    let verify = |x, y| {
+        if x >= 0 && x < width && y >= 0 && y < height {
+            Some((x, y))
+        } else {
+            None
+        }
+    };
+    let ab = (b.0 - a.0, b.1 - a.1);
+    let get_c = |i: i64| verify(a.0 - ab.0 * i, a.1 - ab.1 * i);
+    let get_d = |i: i64| verify(b.0 + ab.0 * i, b.1 + ab.1 * i);
+
+    let mut v = vec![];
+
+    let mut ci = 0;
+    while let Some(c) = get_c(ci) {
+        v.push(c);
+        ci += 1;
+    }
+    let mut di = 0;
+    while let Some(d) = get_d(di) {
+        v.push(d);
+        di += 1;
+    }
+    v
+}
+
 fn main() {
     let grid = Grid::from(
         io::stdin()
@@ -93,4 +120,19 @@ fn main() {
         .collect::<HashSet<_>>();
     let r1 = antinodes.len();
     println!("{}", r1);
+
+    let antinodes2 = antennas
+        .keys()
+        .flat_map(|k| {
+            antennas
+                .get(k)
+                .unwrap()
+                .iter()
+                .tuple_combinations()
+                .flat_map(|(a, b)| get_antinodes2(*a, *b, grid.width, grid.height))
+                .collect_vec()
+        })
+        .collect::<HashSet<_>>();
+    let r2 = antinodes2.len();
+    println!("{}", r2);
 }
