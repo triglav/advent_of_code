979b5fab46b2f5abb6264c5f0373abeb8b0b8a16
Matej Svec <matej@msvec.sk>
2024-12-08 20:22:38 +0100

    Day 6 (2024) - part 2

--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -2,6 +2,7 @@ use std::{collections::HashSet, fmt, io};
 
 use itertools::iproduct;
 
+#[derive(Clone)]
 struct Grid<T> {
     pub width: i64,
     pub height: i64,
@@ -43,6 +44,12 @@ where
         assert!(x >= 0 && x < self.width);
         assert!(y >= 0 && y < self.height);
         self.tiles[(y * self.width + x) as usize]
+    }
+
+    pub fn get_mut(&mut self, x: i64, y: i64) -> &mut T {
+        assert!(x >= 0 && x < self.width);
+        assert!(y >= 0 && y < self.height);
+        self.tiles.get_mut((y * self.width + x) as usize).unwrap()
     }
 
     pub fn find(&self, t: T) -> Option<(i64, i64)> {
@@ -98,29 +105,47 @@ struct Guard {
     dir: Direction,
 }
 
+enum AdvanceResult {
+    Ok(Guard),
+    OutOfBounds,
+    LoopDetected,
+}
+
 impl Guard {
     pub fn new(x: i64, y: i64, dir: Direction) -> Guard {
         Guard { x, y, dir }
     }
 
-    pub fn advance(&self, grid: &Grid<char>, visited: &mut HashSet<(i64, i64)>) -> Option<Guard> {
-        get_direction_coord(grid, self.x, self.y, self.dir).map(move |(x, y)| {
+    pub fn advance(
+        &self,
+        grid: &Grid<char>,
+        visited: &mut HashSet<(i64, i64)>,
+        loop_guard: &mut HashSet<(i64, i64, Direction)>,
+    ) -> AdvanceResult {
+        loop_guard.insert((self.x, self.y, self.dir));
+        if let Some((x, y)) = get_direction_coord(grid, self.x, self.y, self.dir) {
             let t = grid.get(x, y);
             if t == '#' {
-                Guard {
+                AdvanceResult::Ok(Guard {
                     x: self.x,
                     y: self.y,
                     dir: self.dir.right(),
-                }
+                })
             } else {
+                if loop_guard.contains(&(x, y, self.dir)) {
+                    return AdvanceResult::LoopDetected;
+                }
                 visited.insert((x, y));
-                Guard {
+                loop_guard.insert((x, y, self.dir));
+                AdvanceResult::Ok(Guard {
                     x,
                     y,
                     dir: self.dir,
-                }
+                })
             }
-        })
+        } else {
+            AdvanceResult::OutOfBounds
+        }
     }
 }
 
@@ -133,14 +158,44 @@ fn main() {
     );
 
     let (x, y) = grid.find('^').unwrap();
-    let mut guard = Guard::new(x, y, Direction::Up);
+    let guard0 = Guard::new(x, y, Direction::Up);
+
+    let mut guard = guard0.clone();
     let mut visited = HashSet::new();
+    let mut loop_guard = HashSet::new();
+    let mut todo = HashSet::new();
     loop {
-        guard = match guard.advance(&grid, &mut visited) {
-            Some(guard) => guard,
-            None => break,
+        guard = match guard.advance(&grid, &mut visited, &mut loop_guard) {
+            AdvanceResult::Ok(guard) => guard,
+            AdvanceResult::OutOfBounds => break,
+            AdvanceResult::LoopDetected => panic!(),
         };
+        todo.insert((guard.x, guard.y));
     }
     let r1 = visited.len();
     println!("{}", r1);
+
+    let r2 = todo
+        .into_iter()
+        .map(|c| {
+            let mut grid2 = grid.clone();
+            if grid2.get(c.0, c.1) == '^' {
+                return false;
+            }
+            *grid2.get_mut(c.0, c.1) = '#';
+            let mut guard2 = guard0.clone();
+            let mut visited2 = HashSet::new();
+            let mut loop_guard2 = HashSet::new();
+            loop {
+                guard2 = match guard2.advance(&grid2, &mut visited2, &mut loop_guard2) {
+                    AdvanceResult::Ok(guard) => guard,
+                    AdvanceResult::OutOfBounds => break,
+                    AdvanceResult::LoopDetected => return true,
+                };
+            }
+            false
+        })
+        .filter(|&b| b)
+        .count();
+    println!("{}", r2);
 }
