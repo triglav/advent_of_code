7a82dab3c4a640812d60dba527e62a462424b3e6
Matej Svec <matej@msvec.sk>
2024-12-11 21:33:57 +0100

    Day 11 (2024) - part 2

--- 2024/day11/src/main.rs
+++ 2024/day11/src/main.rs
@@ -1,4 +1,10 @@
-use std::io;
+use std::{collections::HashMap, io};
+
+#[derive(Debug)]
+struct Rock {
+    value: u64,
+    count: u64,
+}
 
 fn blink_rock(rock: u64) -> Vec<u64> {
     if rock == 0 {
@@ -13,21 +19,51 @@ fn blink_rock(rock: u64) -> Vec<u64> {
     vec![rock * 2024]
 }
 
-fn blink(rocks: Vec<u64>) -> Vec<u64> {
-    rocks.into_iter().flat_map(blink_rock).collect()
+fn compact(rocks: Vec<Rock>) -> Vec<Rock> {
+    rocks
+        .into_iter()
+        .fold(HashMap::new(), |mut acc, rock| {
+            acc.entry(rock.value)
+                .and_modify(|c| *c += rock.count)
+                .or_insert(rock.count);
+            acc
+        })
+        .into_iter()
+        .map(|(value, count)| Rock { value, count })
+        .collect()
+}
+
+fn blink(rocks: Vec<Rock>) -> Vec<Rock> {
+    compact(rocks)
+        .into_iter()
+        .flat_map(|r| {
+            blink_rock(r.value).into_iter().map(move |rr| Rock {
+                value: rr,
+                count: r.count,
+            })
+        })
+        .collect()
 }
 
 fn main() {
     let input = io::stdin().lines().next().unwrap().unwrap();
     let rocks = input
         .split_whitespace()
-        .map(|s| s.parse::<u64>().unwrap())
+        .map(|s| Rock {
+            value: s.parse::<u64>().unwrap(),
+            count: 1,
+        })
         .collect::<Vec<_>>();
 
     let mut rocks = rocks;
     for _ in 0..25 {
         rocks = blink(rocks);
     }
-    let r1 = rocks.len();
+    let r1 = rocks.iter().map(|r| r.count).sum::<u64>();
     println!("{}", r1);
+    for _ in 0..50 {
+        rocks = blink(rocks);
+    }
+    let r2 = rocks.iter().map(|r| r.count).sum::<u64>();
+    println!("{}", r2);
 }
