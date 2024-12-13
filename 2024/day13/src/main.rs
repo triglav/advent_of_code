09efb52f38f2f5d11916d23fec0d04e3898d936d
Matej Svec <matej@msvec.sk>
2024-12-13 23:28:14 +0100

    Day 13 (2024) - part 2

--- 2024/day13/src/main.rs
+++ 2024/day13/src/main.rs
@@ -8,8 +8,8 @@ use regex::Regex;
 
 #[derive(Debug, Clone, Copy)]
 struct Coords {
-    x: u64,
-    y: u64,
+    x: i64,
+    y: i64,
 }
 
 impl Add for Coords {
@@ -56,10 +56,10 @@ impl Mul for Coords {
     }
 }
 
-impl Mul<u64> for Coords {
+impl Mul<i64> for Coords {
     type Output = Self;
 
-    fn mul(self, rhs: u64) -> Self::Output {
+    fn mul(self, rhs: i64) -> Self::Output {
         Self {
             x: self.x * rhs,
             y: self.y * rhs,
@@ -67,10 +67,10 @@ impl Mul<u64> for Coords {
     }
 }
 
-impl Div<u64> for Coords {
+impl Div<i64> for Coords {
     type Output = Self;
 
-    fn div(self, rhs: u64) -> Self::Output {
+    fn div(self, rhs: i64) -> Self::Output {
         Self {
             x: self.x / rhs,
             y: self.y / rhs,
@@ -82,8 +82,8 @@ fn parse_button(s: &str) -> Coords {
     let re = Regex::new(r"Button [AB]: X\+(\d+), Y\+(\d+)").unwrap();
     re.captures(s)
         .map(|m| Coords {
-            x: m[1].parse::<u64>().unwrap(),
-            y: m[2].parse::<u64>().unwrap(),
+            x: m[1].parse::<i64>().unwrap(),
+            y: m[2].parse::<i64>().unwrap(),
         })
         .unwrap()
 }
@@ -92,48 +92,73 @@ fn parse_prize(s: &str) -> Coords {
     let re = Regex::new(r"Prize: X=(\d+), Y=(\d+)").unwrap();
     re.captures(s)
         .map(|m| Coords {
-            x: m[1].parse::<u64>().unwrap(),
-            y: m[2].parse::<u64>().unwrap(),
+            x: m[1].parse::<i64>().unwrap(),
+            y: m[2].parse::<i64>().unwrap(),
         })
         .unwrap()
 }
 
-// 3 tokens button A
-// 1 token button B
-fn solve1(a: Coords, b: Coords, p: Coords) -> Option<u64> {
-    let a_count = p / a;
-    let max_a_count = a_count.x.min(a_count.y).min(100);
+fn solve(a: Coords, b: Coords, p: Coords) -> Option<(i64, i64)> {
+    let det = a.x * b.y - a.y * b.x;
+    if det == 0 {
+        println!("none");
+        return None;
+    }
+    let aa = b.y * p.x - b.x * p.y;
+    let a_c = aa / det;
+    if aa % det == 0 && a_c >= 0 {
+        let bb = a.x * p.y - a.y * p.x;
+        let b_c = bb / det;
+        if bb % det == 0 && b_c >= 0 {
+            return Some((a_c, b_c));
+        }
+    }
+    None
+}
 
-    (0..=max_a_count)
-        .filter_map(|c_a| {
-            let p2 = p - (a * c_a);
-            let c_b = p2 / b;
-            if c_b.x != c_b.y {
-                return None;
-            }
-            let c_b = c_b.x;
-            let p2 = a * c_a + b * c_b;
-            if p2.x != p.x || p2.y != p.y {
-                return None;
-            }
-            Some((c_a, c_b))
-        })
-        .map(|(a, b)| 3 * a + b)
-        .min()
+fn solve1(a: Coords, b: Coords, p: Coords) -> Option<i64> {
+    if let Some((ac, bc)) = solve(a, b, p) {
+        if ac <= 100 && bc <= 100 {
+            return Some(3 * ac + bc);
+        }
+    }
+    None
+}
+
+fn solve2(a: Coords, b: Coords, p: Coords) -> Option<i64> {
+    let p = Coords {
+        x: p.x + 10000000000000,
+        y: p.y + 10000000000000,
+    };
+    if let Some((ac, bc)) = solve(a, b, p) {
+        Some(3 * ac + bc)
+    } else {
+        None
+    }
 }
 
 fn main() {
-    let input = io::stdin().lines().map(|l| l.unwrap());
-    let r1 = input
+    let input = io::stdin()
+        .lines()
+        .map(|l| l.unwrap())
         .chunks(4)
         .into_iter()
-        .filter_map(|chunk| {
+        .map(|chunk| {
             let c = chunk.collect_vec();
             let a = parse_button(&c[0]);
             let b = parse_button(&c[1]);
             let p = parse_prize(&c[2]);
-            solve1(a, b, p)
+            (a, b, p)
         })
-        .sum::<u64>();
+        .collect_vec();
+    let r1 = input
+        .iter()
+        .filter_map(|&(a, b, p)| solve1(a, b, p))
+        .sum::<i64>();
     println!("{}", r1);
+    let r2 = input
+        .iter()
+        .filter_map(|&(a, b, p)| solve2(a, b, p))
+        .sum::<i64>();
+    println!("{}", r2);
 }
