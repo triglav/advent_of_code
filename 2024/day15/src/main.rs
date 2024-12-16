7fe3889942c6eac5980c0daad6dc9c64164f251a
Matej Svec <matej@msvec.sk>
2024-12-16 23:16:00 +0100

    Day 15 (2024) - part 2

--- 2024/day15/src/main.rs
+++ 2024/day15/src/main.rs
@@ -3,7 +3,7 @@ use std::{fmt, io};
 
 use itertools::{iproduct, Itertools};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Direction {
     Up,
     Down,
@@ -23,17 +23,38 @@ fn parse_direction(c: char) -> Direction {
 
 fn direction_vector(d: Direction) -> Coords {
     match d {
-        Direction::Up => Coords { x: 0, y: -1 },
-        Direction::Down => Coords { x: 0, y: 1 },
-        Direction::Left => Coords { x: -1, y: 0 },
-        Direction::Right => Coords { x: 1, y: 0 },
+        Direction::Up => Coords::up(),
+        Direction::Down => Coords::down(),
+        Direction::Left => Coords::left(),
+        Direction::Right => Coords::right(),
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Clone, Copy)]
 struct Coords {
     x: i64,
     y: i64,
+}
+
+impl Coords {
+    pub fn up() -> Self {
+        Coords { x: 0, y: -1 }
+    }
+    pub fn down() -> Self {
+        Coords { x: 0, y: 1 }
+    }
+    pub fn left() -> Self {
+        Coords { x: -1, y: 0 }
+    }
+    pub fn right() -> Self {
+        Coords { x: 1, y: 0 }
+    }
+}
+
+impl fmt::Debug for Coords {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
 }
 
 impl Add for Coords {
@@ -170,6 +191,13 @@ fn gps_coords(c: Coords) -> i64 {
     100 * c.y + c.x
 }
 
+fn move_tile(grid: &mut Grid<char>, p: Coords, d: Coords) {
+    let t = grid.get(p);
+    assert_eq!(grid.get(d), '.');
+    *grid.get_mut(d) = t;
+    *grid.get_mut(p) = '.';
+}
+
 fn push(grid: &mut Grid<char>, p: Coords, d: Direction) -> Option<Coords> {
     let v = direction_vector(d);
     let dest = p.add(v);
@@ -177,16 +205,69 @@ fn push(grid: &mut Grid<char>, p: Coords, d: Direction) -> Option<Coords> {
     match dest_tile {
         '#' => None,
         '.' => {
-            let t = grid.get(p);
-            *grid.get_mut(dest) = t;
-            *grid.get_mut(p) = '.';
+            move_tile(grid, p, dest);
             Some(dest)
         }
-        'O' => match push(grid, dest, d) {
+        'O' | '[' | ']' => match push(grid, dest, d) {
             Some(_) => push(grid, p, d),
             None => None,
         },
         _ => panic!("Unexpected tile"),
+    }
+}
+
+fn push_box(
+    grid: &mut Grid<char>,
+    p: Coords,
+    p2: Coords,
+    d: Direction,
+    dry: bool,
+) -> Option<Coords> {
+    let left = push2(grid, p, d, dry);
+    let right = push2(grid, p2, d, dry);
+
+    match (left, right) {
+        (Some(left), Some(right)) => {
+            if !dry {
+                move_tile(grid, p, left);
+                move_tile(grid, p2, right);
+            }
+            Some(p)
+        }
+        _ => None,
+    }
+}
+
+fn push2(grid: &mut Grid<char>, p: Coords, d: Direction, dry: bool) -> Option<Coords> {
+    if d == Direction::Left || d == Direction::Right {
+        return push(grid, p, d);
+    }
+    let v = direction_vector(d);
+    let dest = p.add(v);
+    let dest_tile = grid.get(dest);
+    match dest_tile {
+        '#' => None,
+        '.' => Some(dest),
+        '[' => push_box(grid, dest, dest.add(Coords::right()), d, dry),
+        ']' => push_box(grid, dest, dest.add(Coords::left()), d, dry),
+        _ => panic!("Unexpected tile"),
+    }
+}
+
+fn transform_to_wide_grid(grid: &Grid<char>) -> Grid<char> {
+    let tiles = iproduct!(0..grid.height, 0..grid.width)
+        .map(|(y, x)| grid.get(Coords { x, y }))
+        .flat_map(|t| match t {
+            '#' | '.' => [t, t],
+            '@' => ['@', '.'],
+            'O' => ['[', ']'],
+            _ => panic!("Unexpected tile"),
+        })
+        .collect_vec();
+    Grid {
+        width: grid.width * 2,
+        height: grid.height,
+        tiles,
     }
 }
 
@@ -195,12 +276,13 @@ fn main() {
     let (grid_lines, moves_lines): (Vec<_>, Vec<_>) =
         lines.into_iter().partition(|l| l.starts_with('#'));
 
-    let mut grid = Grid::from(
+    let grid = Grid::from(
         grid_lines
             .into_iter()
             .map(|l| l.chars().collect::<Vec<_>>())
             .collect::<Vec<_>>(),
     );
+    let grid2 = transform_to_wide_grid(&grid);
     let moves = moves_lines
         .into_iter()
         .join("")
@@ -208,19 +290,41 @@ fn main() {
         .map(parse_direction)
         .collect_vec();
 
-    let mut robot_pos = grid.find('@').unwrap();
-    *grid.get_mut(robot_pos) = '.';
+    {
+        let mut grid = grid;
+        let mut robot_pos = grid.find('@').unwrap();
+        *grid.get_mut(robot_pos) = '.';
 
-    for m in moves {
-        if let Some(new_pos) = push(&mut grid, robot_pos, m) {
-            robot_pos = new_pos;
+        for m in moves.iter() {
+            if let Some(new_pos) = push(&mut grid, robot_pos, *m) {
+                robot_pos = new_pos;
+            }
         }
-    }
 
-    let r1 = iproduct!(0..grid.height, 0..grid.width)
-        .map(|(y, x)| Coords { x, y })
-        .filter(move |&c| grid.get(c) == 'O')
-        .map(gps_coords)
-        .sum::<i64>();
-    println!("{}", r1);
+        let r1 = iproduct!(0..grid.height, 0..grid.width)
+            .map(|(y, x)| Coords { x, y })
+            .filter(move |&c| grid.get(c) == 'O')
+            .map(gps_coords)
+            .sum::<i64>();
+        println!("{}", r1);
+    }
+    {
+        let mut grid = grid2;
+        let mut robot_pos = grid.find('@').unwrap();
+        *grid.get_mut(robot_pos) = '.';
+
+        for m in moves {
+            if let Some(new_pos) = push2(&mut grid, robot_pos, m, true) {
+                push2(&mut grid, robot_pos, m, false);
+                robot_pos = new_pos;
+            }
+        }
+
+        let r2 = iproduct!(0..grid.height, 0..grid.width)
+            .map(|(y, x)| Coords { x, y })
+            .filter(move |&c| grid.get(c) == '[')
+            .map(gps_coords)
+            .sum::<i64>();
+        println!("{}", r2);
+    }
 }
