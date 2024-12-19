13cdb18c2367e499d8eaaa86a7c792f2e586d01f
Matej Svec <matej@msvec.sk>
2024-12-19 16:30:18 +0100

    zz2

--- 2024/day19/src/main.rs
+++ 2024/day19/src/main.rs
@@ -25,12 +25,10 @@ fn solve(towels: &Vec<&str>, design: &str) -> HashSet<String> {
         if !design.starts_with(d.as_str()) {
             continue;
         }
-        if seen.contains(p.as_str()) {
-            // println!("seen: {}", p);
+        if seen.contains(d.as_str()) {
             continue;
         }
-        // println!("{}", p);
-        seen.insert(p.clone());
+        seen.insert(d.clone());
         for &t in towels.iter() {
             let d2 = format!("{}{}", d, t);
             if design.starts_with(d2.as_str()) {
@@ -45,10 +43,34 @@ fn solve(towels: &Vec<&str>, design: &str) -> HashSet<String> {
     solutions
 }
 
+fn combos(towel_combos: &HashMap<String, HashSet<String>>, solution: &str) -> HashSet<String> {
+    let mut cs = HashSet::new();
+    let mut todo = vec![("".to_string(), solution.split('-').collect::<Vec<_>>())];
+    while let Some((pattern, mut solution)) = todo.pop() {
+        if let Some(t) = solution.pop() {
+            if let Some(tc) = towel_combos.get(t) {
+                tc.iter().for_each(|towel| {
+                    let pattern2 = if pattern.is_empty() {
+                        towel.to_string()
+                    } else {
+                        format!("{}-{}", towel, pattern)
+                    };
+                    todo.push((pattern2, solution.clone()));
+                });
+            }
+        } else {
+            cs.insert(pattern.to_string());
+        }
+    }
+    // println!("{:?}", cs);
+    cs
+}
+
 fn main() {
     let lines = io::stdin().lines().map(|l| l.unwrap()).collect::<Vec<_>>();
 
-    let towels = lines[0].split(",").map(|x| x.trim()).collect::<Vec<_>>();
+    let mut towels = lines[0].split(",").map(|x| x.trim()).collect::<Vec<_>>();
+    towels.sort_by_key(|a| a.len());
     let designs = lines[2..].iter().collect::<Vec<_>>();
 
     let towel_combos = towels
@@ -64,21 +86,42 @@ fn main() {
     let r1 = solutions.iter().filter(|&s| !s.is_empty()).count();
     println!("{}", r1);
 
+    // let r2 = solutions
+    //     .iter()
+    //     // .inspect(|z| println!("solution: {:?}", z))
+    //     .map(|s| {
+    //         s.iter()
+    //             // .inspect(|z| println!("* {}", z))
+    //             .map(|solution| {
+    //                 solution
+    //                     .split('-')
+    //                     .map(|ss| towel_combos.get(ss).map_or(0, |s| s.len()))
+    //                     // .inspect(|z| println!("  -> {}", z))
+    //                     .product::<usize>()
+    //             })
+    //             .sum::<usize>()
+    //     })
+    //     .collect::<Vec<_>>();
     let r2 = solutions
         .iter()
-        // .inspect(|z| println!("solution: {:?}", z))
-        .map(|s| {
-            s.iter()
-                // .inspect(|z| println!("* {}", z))
-                .map(|solution| {
-                    solution
-                        .split('-')
-                        .map(|ss| towel_combos.get(ss).map_or(0, |s| s.len()))
-                        // .inspect(|z| println!("  -> {}", z))
-                        .product::<usize>()
-                })
-                .sum::<usize>()
+        .inspect(|solutions| println!("solution: {:?}", solutions))
+        .map(|solutions| {
+            solutions
+                .iter()
+                .map(|s| combos(&towel_combos, s))
+                // .inspect(|c| println!("  -> {}", c))
+                .fold(HashSet::new(), |acc, c| acc.union(&c).cloned().collect())
+            // s.iter().inspect(|z| println!("* {}", z))
+            // .map(|solution| {
+            //     solution
+            //         .split('-')
+            //         .map(|ss| towel_combos.get(ss).map_or(0, |s| s.len()))
+            //         // .inspect(|z| println!("  -> {}", z))
+            //         .product::<usize>()
+            // })
+            // .sum::<usize>()
         })
+        .inspect(|c| println!("  -> {:?} ({})", c, c.len()))
         .collect::<Vec<_>>();
     println!("{:?}", r2);
 }
