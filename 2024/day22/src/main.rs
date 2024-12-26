d53e8f6ac9674d4f05256eda594cd533ff32dcf5
Matej Svec <matej@msvec.sk>
2024-12-26 20:47:24 +0100

    Day 22 (2024) - part 2

--- 2024/day22/src/main.rs
+++ 2024/day22/src/main.rs
@@ -1,4 +1,6 @@
-use std::io;
+use std::{collections::HashMap, io};
+
+use itertools::{self, Itertools};
 
 fn mix(n: usize, secret: usize) -> usize {
     n ^ secret
@@ -22,6 +24,10 @@ fn next_number_acc(secret: usize, count: usize) -> usize {
     secret
 }
 
+fn price(secret: usize) -> usize {
+    secret % 10
+}
+
 fn main() {
     let lines = io::stdin()
         .lines()
@@ -32,4 +38,36 @@ fn main() {
         .map(|&n| next_number_acc(n, 2000))
         .sum::<usize>();
     println!("{}", r1);
+
+    let r2 = lines
+        .iter()
+        .flat_map(|&secret| {
+            (0..2000)
+                .scan(secret, |secret, _| {
+                    let prev_secret = *secret;
+                    *secret = next_number(*secret);
+                    Some(prev_secret)
+                })
+                .map(price)
+                .tuple_windows::<(_, _)>()
+                .map(|(a, b)| (b, b as isize - a as isize))
+                .tuple_windows::<(_, _, _, _)>()
+                .map(|(a, b, c, d)| {
+                    let combo = (a.1, b.1, c.1, d.1);
+                    let price = d.0;
+                    (combo, price)
+                })
+                .unique_by(|&(combo, _)| combo)
+        })
+        .fold(HashMap::new(), |mut acc, (combo, price)| {
+            acc.entry(combo)
+                .and_modify(|e| *e += price)
+                .or_insert(price);
+            acc
+        })
+        .into_iter()
+        .max_by_key(|&(_, price)| price)
+        .unwrap()
+        .1;
+    println!("{}", r2);
 }
